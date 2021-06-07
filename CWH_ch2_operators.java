067ce331b4d6573f8e7a1b5fb7450cc46f6c9726
Pushpraj2001 <19bca1099@cuchd.in>
2021-06-08 00:33:44 +0530

    update

---
+++ CWH_ch2_operators.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class CWH_ch2_operators{
+
+public static void main(String[] args) {
+    
+    // arthematic operators
+         int a=4;
+    //  int b = a % 5; //module operator
+    // System.out.println(b);
+        //Assignment operator
+            int b =9;
+            b*= 3 ;
+            System.out.println(b);
+
+            //Comparison operators
+            // System.out.println(65<65);
+
+            // logical operator
+            System.out.println(65>5 && 89>69);//&& means that all the statement should be correct.
+
+            // bitwise operators
+            System.out.println(2&3);
+            /* 
+            10
+            11
+            ---
+            10
+            
+            */ 
+
+ 
+
+}
+}
