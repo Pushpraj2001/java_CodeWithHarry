3af72fe803e9901fa22a72493c0a02feb80d7f65
Pushpraj2001 <19bca1099@cuchd.in>
2021-06-07 23:59:16 +0530

    ps done

---
+++ CWH_ch1_ps.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class CWH_ch1_ps {
+
+    public static void main(String[] args) {
+        // question no 1
+        // sum of three no 
+
+       /* int a= 65;
+        int b= 56;
+        int c = 85;
+        int sum = a + b +c;
+        System.out.println(sum);*/
+
+        // Question no 2
+        // WAP to calculate CGPA using marks of three subject
+       
+       /* int subject1 = 56;
+        int subject2 = 86;
+        int subject3 = 68;
+        int cgpa = (subject1 + subject2 + subject3)/3;
+
+        System.out.println(cgpa);*/
+
+
+        // 4. WAP to calculate Kilometer into meter 
+          
+        /*int Kilometer1 = 26;
+        int meter = Kilometer1*1000;
+
+        System.out.println(meter);*/
+        
+        // WAP which asks the user to enter his'her name and greets them with "hello<name>,have a good day" text
+
+        // Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+
+        // System.out.println("Hello "+input+",have a good day" );
+// WAP to detect whethir a no entered by the user is integer or not
+Scanner scanner = new Scanner(System.in);
+        
+        System.out.println(scanner.hasNextInt());
+        // hasNextInt is used to determine the user has entered the int value or not
+    }
+} 
